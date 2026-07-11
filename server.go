5efc40bc0b37e28b7c901e195e0980ce54726051
richardnascimento18 <computer.richard0923@gmail.com>
2026-07-11 11:33:56 -0300

    feat: create dummy golang server

---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", Hello)
+
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	log.Fatal(err)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello FullCycle</h1>"))
+}
