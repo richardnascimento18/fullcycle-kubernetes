30f5d7cd75ccf03462de622e0608395bef7092a3
richardnascimento18 <computer.richard0923@gmail.com>
2026-07-13 09:57:28 -0300

    feat: add liveness probe to k8s

--- server.go
+++ server.go
@@ -5,16 +5,32 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+var startedAt = time.Now()
 
 func main() {
 	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/healthz", Healthz)
 	http.HandleFunc("/", Hello)
 
 	log.Println("Starting server on :8080")
 	err := http.ListenAndServe(":8080", nil)
 	log.Fatal(err)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() > 25 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
